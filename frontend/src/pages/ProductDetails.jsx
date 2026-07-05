5d9b15f713df25df4ef74a041eb6bdb1377ee7a9
saptarshisau <saptarshisau09@gmail.com>
2026-07-05 21:19:53 +0530

    incQuantity and decQuantiy feature

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../pageStyles/ProductDetails.css'
 import PageTitle from '../components/PageTitle';
 import Navbar from '../components/Navbar';
